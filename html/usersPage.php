2613895b07537e8825e00291f3d22d4574150192
amahdavid <amahdavid34@gmail.com>
2024-03-26 16:53:09 -0700

    Chat-App: Logout feature completed

--- html/usersPage.php
+++ html/usersPage.php
@@ -30,7 +30,7 @@
                     <p><?php echo $row['status']?></p>
                 </div>
             </div>
-            <a href="#" class="logout">Logout</a>
+            <a href="../php/logout.php?user_id=<?php echo $row['unique_id'] ?>" class="logout">Logout</a>
         </header>
         <div class="search">
             <span class="text">Select a user or group to start chatting</span>
--- javascript/chat.js
+++ javascript/chat.js
@@ -14,11 +14,20 @@ sendBtn.onclick = () => {
         if(xhr.readyState === XMLHttpRequest.DONE){
             if(xhr.status === 200){
                 inputField.value = ""; // Clear the input field
+                scrollToBottom();
             }
         }
     }
     let formData = new FormData(form);
     xhr.send(formData);
+}
+
+chatBox.onmouseenter = () => {
+    chatBox.classList.add("active");
+} 
+
+chatBox.onmouseleave = () => {
+    chatBox.classList.remove("active");
 }
 
 setInterval(() => {
@@ -29,7 +38,9 @@ setInterval(() => {
             if(xhr.status === 200){
                 let data = xhr.response;
                 chatBox.innerHTML = data;
-                // console.log("sent");
+                if(!chatBox.classList.contains("active")){
+                    scrollToBottom();
+                }
             }
         }
     }
@@ -37,3 +48,7 @@ setInterval(() => {
     xhr.send(formData);
 }
 , 500);
+
+function scrollToBottom(){
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
